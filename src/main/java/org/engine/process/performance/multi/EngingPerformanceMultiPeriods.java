259c12a8af0a2bc07a51230d38b4c5bbc21f7ae0
assafsh2 <assafsh2@gmail.com>
2017-07-18 14:09:47 +0300

    fix

--- src/main/java/org/engine/process/performance/multi/EngingPerformanceMultiPeriods.java
+++ src/main/java/org/engine/process/performance/multi/EngingPerformanceMultiPeriods.java
@@ -16,8 +16,8 @@ public class EngingPerformanceMultiPeriods extends InnerService {
 	private String kafkaAddress;
 	private String schemaRegustryUrl;  
 	private String sourceName; 
-	private int num_of_periods = 10;
-	private int num_of_updates = 10;
+	private int num_of_periods;
+	private int num_of_updates;
 	
 	public EngingPerformanceMultiPeriods(String kafkaAddress,
 			String schemaRegistryUrl, String schemaRegistryIdentity,String sourceName) {
@@ -25,10 +25,12 @@ public class EngingPerformanceMultiPeriods extends InnerService {
 		this.kafkaAddress = kafkaAddress; 
 		this.sourceName = sourceName;
 		System.out.println("NUM_OF_PERIODS::::::::" + System.getenv("NUM_OF_PERIODS")); 
-		System.out.println("NUM_OF_UPDATES::::::::" + System.getenv("NUM_OF_UPDATES")); 
-	
+		System.out.println("AANUM_OF_UPDATES::::::::" + System.getenv("NUM_OF_UPDATES")); 
+	System.out.println("GGG"); 
 		num_of_periods = Integer.parseInt(System.getenv("NUM_OF_PERIODS"));
+		System.out.println("AAA"); 
 		num_of_updates = Integer.parseInt(System.getenv("NUM_OF_UPDATES"));
+		System.out.println("BBB"); 
 	}
 
 	@Override
@@ -52,26 +54,30 @@ public class EngingPerformanceMultiPeriods extends InnerService {
 
 	@Override
 	protected ServiceStatus execute() throws Exception {
-
+		System.out.println("CCC"); 
 		double lat = 4.3;
 		double longX = 6.4;
 
 		for( int i = 0; i < num_of_periods; i++ ) {
+			
+			System.out.println("PERDIOD " + i); 
 
 			String externalSystemID = utils.randomExternalSystemID();
 			SinglePeriod singlePeriod = new SinglePeriod(); 	
 
 			for(int j = 0 ; j < num_of_updates; j++) {
+				System.out.println("UPDATE " + j);
 				
 				Date startTime = new Date(System.currentTimeMillis());
 				String latStr = Double.toString(lat);
 				String longXStr = Double.toString(longX);
 
 				HandlePerformanceMessages handlePerformanceMessages = new HandlePerformanceMessages(kafkaAddress,schemaRegustryUrl,
-																									sourceName,externalSystemID,latStr,longXStr);					
+										sourceName,externalSystemID,latStr,longXStr);					
+				System.out.println("Before  handleMessage " + j);
 				handlePerformanceMessages.handleMessage(); 
 				singlePeriod.addMessageData(new MessageData(startTime,externalSystemID,latStr, longXStr, sourceName,handlePerformanceMessages));
-
+				System.out.println(singlePeriod.getMessageDataList());
 				lat = lat * 1.3;
 				longX = longX * 2.3;
 				
